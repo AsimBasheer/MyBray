3d75ab1b45495aab9023a9eaa24775d110e6bf58
ASimMehar21 <asimmehar124@gmail.com>
2024-06-03 22:28:32 +0500

    Initial Setup

---
+++ server.js
@@ -0,0 +1,25 @@
+if (process.env.NODE_ENV !== "prodcution") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(expressLayouts); // Move this line up to set the layout directory
+app.set("layout", "layouts/layout"); // Set the layout file
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Coonected to Mongoose"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
