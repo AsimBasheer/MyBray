2837e598b9bfabaf76670a60cd767c44d1abc657
ASimMehar21 <asimmehar124@gmail.com>
2024-06-04 01:55:28 +0500

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== "prodcution") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(expressLayouts); // Move this line up to set the layout directory
 app.set("layout", "layouts/layout"); // Set the layout file
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {});
@@ -21,5 +24,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Coonected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
